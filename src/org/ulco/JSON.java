d609d6a7a6c2058112f79fce9961f33eb4071c17
simonansel <contact@simon-ansel.fr>
2017-11-16 14:06:02 +0100

    Add step 2

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -1,18 +1,18 @@
 package org.ulco;
-
+import java.lang.reflect.Constructor;
 public class JSON {
     static public GraphicsObject parse(String json) {
         GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
         String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
-
-        if (type.compareTo("square") == 0) {
-            o = new Square(str);
-        } else if (type.compareTo("rectangle") == 0) {
-            o = new Rectangle(str);
-        } else if (type.compareTo("circle") == 0) {
-            o = new Circle(str);
+        try {
+            Class<?> classe = Class.forName("org.ulco."+type.substring(0,1).toUpperCase()+type.substring(1));
+            Constructor<?> constructor = classe.getConstructor(String.class);
+            o = (GraphicsObject)constructor.newInstance(str);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
         return o;
     }
 
